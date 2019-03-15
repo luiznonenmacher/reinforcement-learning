736b463775aea245fd08399c5cd68ebd912f2c44
Luiz Nonenmacher <ljuniornone@gmail.com>
2019-03-15 02:20:11 +0000

    Finalized version of the project

---
+++ quadcopter/takeoff_task.py
@@ -0,0 +1,67 @@
+import numpy as np
+from physics_sim import PhysicsSim
+
+class Task():
+    """Task (environment) that defines the goal and provides feedback to the agent."""
+    def __init__(self, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=1., target_pos=None):
+        """Initialize a Task object.
+        Params
+        ======
+            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
+            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
+            init_angle_velocities: initial radians/second for each of the three Euler angles
+            runtime: time limit for each episode
+            target_pos: target/goal (x,y,z) position for the agent
+        """
+        
+        # Goal
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 2.]) 
+        self.init_pos = init_pos if init_pos is not None else np.array([0.0, 0.0, 1.0, 0.0, 0.0, 0.0])     
+        
+      
+        
+        # Simulation
+        self.sim = PhysicsSim(self.init_pos, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat = 3 
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4
+
+        
+    def get_reward(self):
+        """Uses current pose of sim to return reward."""
+        
+        done = False
+        reward = (np.tanh(1 - 0.003*(abs(self.sim.pose[:3] - self.target_pos))).sum()) + 1.5
+       
+        # Rewards for crashing or passing trough the target
+        if self.sim.runtime > self.sim.time:
+            if self.sim.done:
+                reward = reward  -100 #-100
+                done = True
+            elif self.sim.pose[2] >= self.target_pos[2]:
+                reward = reward + 10000 #10000
+                done = True
+        
+        return reward, done
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+        for _ in range(self.action_repeat):
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+            r, done = self.get_reward()
+            reward += r 
+            pose_all.append(self.sim.pose)
+        next_state = np.concatenate(pose_all)
+        return next_state, reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        return state
